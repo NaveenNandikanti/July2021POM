b7de9bca5e686ca704bc7d894d8dd0632da61629
srimoksham <srimoksham@srimokshams-MacBook-Air.local>
2021-10-19 15:23:08 +0530

    Home page feature been added

---
+++ src/main/java/com/qa/opencart/pages/HomePage.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+public class HomePage {
+	
+	public HomePage() {
+		System.out.println("This is home page feature");
+	}
+	
+	public void myHomePage() {
+		System.out.println("This is user specific home page");
+	}
+}
